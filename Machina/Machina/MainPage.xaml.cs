4a7bb58daf45226e073e65b73ec9f791a55bcab6
franckyp1 <franckyp1+rapports_git>
2020-06-06 21:14:19 +0200

    Ajout prise de photo

--- Machina/Machina/MainPage.xaml.cs
+++ Machina/Machina/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -20,7 +21,36 @@ namespace Machina
 
         private void StartButtonClicked(object sender, EventArgs e)
         {
+            StartButtonClickedAsync();
+        }
+       
+        
+        private async Task StartButtonClickedAsync()
+        {
+            await CrossMedia.Current.Initialize();
 
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+               await DisplayAlert("Erreur", ":La caméra n'est pas disponible.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("Reussi", file.Path, "OK");
+
+            /*image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });*/
         }
     }
 }
